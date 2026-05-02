558e520551bbb151d92aba4f4a769f651602aab9
Brandon Bothell <bothellbrandon@gmail.com>
2026-05-02 05:47:26 -0400

    fix: account for motion detection computation time in motion timestamps
    Maybe we should actually measure how long the detectMotion() method took to run and use that in this computation

--- source/camera.ts
+++ source/camera.ts
@@ -205,7 +205,7 @@ export async function setupStreamMotionMonitoring(streamId?: string) {
                 }, 10000) // flush every 10 seconds
               } else {
                 state.currentRecordingMotionTimestamps.push(
-                  now - state.startedRecordingAt,
+                  Math.max(0, now - state.startedRecordingAt - 1000),
                 )
                 logMotion(
                   `Updated motion timestamps in state: ${JSON.stringify(state.currentRecordingMotionTimestamps)}`,
